bd261ba8a19f8a5266c5e14f25d4235260b87321
KamilBorkowski97 <kamilborkowski037@gmail.com>
2019-10-27 21:40:28 +0100

    added logger

--- src/aura/accountToggleLocker/accountToggleLockerController.js
+++ src/aura/accountToggleLocker/accountToggleLockerController.js
@@ -5,19 +5,6 @@
 
     clickAction: function (component, event, helper) {
         component.set("v.acc.Locker_Status__c", "v.locked");
-
-        /*var action = component.get("c.updateAccount");
-        action.setParam({
-            acc: component.get("v.acc"),
-        });
-
-        action.setCallback(this, function (response) {
-
-            var response = response.getReturnValue();
-            component.set("v.acc", response);
-
-        });
-        $A.enqueueAction(action);*/
     },
 
     doInit: function (component, event, helper) {
@@ -48,41 +35,5 @@
         }
 
     },
-
-    /*handleSaveRecord: function (component, event, helper) {
-        //nie działa saveRecord - pokazuje ze nie zna takiej metody
-        component.find("recordEditor").saveRecord($A.getCallback(function (saveResult) {
-            if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
-                console.log("Save completed successfully2.");
-            }
-        }));
-    },
-
-    recordUpdated: function (component, event, helper) {
-        var eventParams = event.getParams();
-        if (eventParams.changeType === "CHANGED") {
-            // get the fields that are changed for this record
-            var changedFields = eventParams.changedFields;
-            console.log('Fields that are changed: ' + JSON.stringify(changedFields));
-            // record is changed so refresh the component (or other component logic)
-            var resultsToast = $A.get("e.force:showToast");
-            resultsToast.setParams({
-                "title": "Saved",
-                "message": "The record was updated."
-            });
-            resultsToast.fire();
-        } else if (eventParams.changeType === "LOADED") {
-            console.log("Record is loaded successfully.");
-        } else if (eventParams.changeType === "REMOVED") {
-            var resultsToast = $A.get("e.force:showToast");
-            resultsToast.setParams({
-                "title": "Deleted",
-                "message": "The record was deleted."
-            });
-            resultsToast.fire();
-        } else if (eventParams.changeType === "ERROR") {
-            console.log('Error: ' + component.get("v.error"));
-        }
-    }*/
 
 });
